ac398e51e1ffc9e1d38efd5af517a17f3426bf43
at695 <72502386+at695@users.noreply.github.com>
2021-02-19 15:06:10 +0000

    Update login_db.py
    Added a get_user_id() function so the user_id can be a session variable which can be accessed by other pages of the application

--- login_db.py
+++ login_db.py
@@ -10,4 +10,15 @@ def get_password_hash(username):
 	passwordHash = result[0][0]
 	return passwordHash
 
-get_password_hash("example")
+def get_user_id(username):
+	con = sl.connect("Execurse.db")
+	cursor = con.cursor()
+	query = """SELECT user_id FROM USER where username = ?"""
+	cursor.execute(query, [username])
+	result = cursor.fetchall()
+	cursor.close()
+	user_id = result[0][0]
+	return user_id
+
+print(get_password_hash("example"))
+print(get_user_id("example"))
